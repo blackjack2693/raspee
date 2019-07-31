06e3da8f104c504fa004e0e47be5e302a55ec618
blackjack2693 <35633229+blackjack2693@users.noreply.github.com>
2019-07-31 22:40:06 +0200

    upload

---
+++ Test.java
@@ -0,0 +1,34 @@
+
+
+/**
+ * Beschreiben Sie hier die Klasse Test.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Test
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private GLKamera k1;
+
+    /**
+     * Konstruktor für Objekte der Klasse Test
+     */
+    public Test()
+    {
+        // Instanzvariable initialisieren
+        k1 = GLKamera (0,0,0);
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
